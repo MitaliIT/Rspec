340f853ecd2ff4b8f3d5fbf35061f14c59b75eb6
MitaliIT <mitaliyuvasoft388@gmail.com>
2023-08-29 14:31:56 +0530

    add product payment with strip

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'blogs#index'
+  # get 'products/index'
+  resources :products do
+    post 'add_to_cart', on: :member
+ end
+  # post '/charge', to: 'charges#create'
+  # root 'product#index'
   resources :blogs
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+    product = Product.create(
+      name: "Laptop",
+      description: "A laptop is a portable computer designed to be used on the go. It usually features a clamshell design with a keyboard and a display screen attached to the top part",
+      price: "100",
+      quantity: "10"
+    )
+    product = Product.create(
+      name: "Bottle",
+      description: "",
+      price: "100",
+      quantity: "5"
+    )
+    product = Product.create(
+      name: "Mobile",
+      description: "A smartphone is a handheld electronic device that combines the functionalities of a mobile phone with those of a computer. It typically features a touchscreen interface",
+      price: "200",
+      quantity: "10"
+    )
+    product = Product.create(
+      name: "Charger",
+      description: "The charger controls the flow of electrical current and voltage to prevent overcharging, which can damage the battery, and to ensure efficient and safe charging.",
+      price: "200",
+      quantity: "5"
+    )
+    product = Product.create(
+      name: "Switch",
+      description: "In the context of computer networking, a switch is a networking device that operates at the data link layer (Layer 2) of the OSI model.",
+      price: "300",
+      quantity: "20"
+    )
+    product = Product.create(
+      name: "Headphone",
+      description: " They provide a private and immersive listening experience by delivering sound directly into the ears",
+      price: "150",
+      quantity: "20"
+    )
+end
